e19eaaadbd3948bbe2d1c2f15ca3988ce5bffcb2
arafat-alim <arafat97alim@gmail.com>
2022-07-14 01:49:03 +0530

    react -confetti dependencies is installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 function App() {
+  //! Creating a state
+  const [tenzies, setTenzies] = React.useState(false);
   return (
     <main>
       <h1>Tenzies</h1>
       <p></p>
       <div className="die-container">die Element</div>
-      <button className="roll-dice">Roll</button>
+      <button className="roll-dice">{tenzies ? "Reset" : "Roll"}</button>
     </main>
   );
 }
