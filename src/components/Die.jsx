719301d92db768a0797cdff0fd6f1f5bf5313e8a
arafat-alim <arafat97alim@gmail.com>
2022-07-14 02:06:17 +0530

    die component styled successfully.

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Die() {
-  return <div>Die Components</div>;
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.held ? "#59E391" : "white",
+  };
+  return (
+    <div className="die-face" style={styles}>
+      <div className="die-num">{PaymentResponse.num}</div>
+    </div>
+  );
 }
